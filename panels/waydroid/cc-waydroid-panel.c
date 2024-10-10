fa22795f133c38b9e6be29a400d93dae90f9575d
Bardia Moshiri <bardia@furilabs.com>
2024-10-10 10:56:33 -0400

    waydroid: free everything in finalize instead of in g idle
    the issue with freeing in g idle is if we don't get a G_SOURCE_REMOVE, it will keep running and it might lead to memory corruption. a check in each function would work too but freeing in finalize seems more correct
    
    Signed-off-by: Bardia Moshiri <bardia@furilabs.com>

--- panels/waydroid/cc-waydroid-panel.c
+++ panels/waydroid/cc-waydroid-panel.c
@@ -62,6 +62,14 @@ cc_waydroid_panel_finalize (GObject *object)
     g_free (self->selected_app_name);
   if (self->selected_app_pkgname != NULL)
     g_free (self->selected_app_pkgname);
+  if (self->waydroid_ip_output != NULL)
+    g_free (self->waydroid_ip_output);
+  if (self->waydroid_vendor_output != NULL)
+    g_free (self->waydroid_vendor_output);
+  if (self->waydroid_version_output != NULL)
+    g_free (self->waydroid_version_output);
+  if (self->apps != NULL)
+    g_strfreev (self->apps);
 
   self->app_widgets = NULL;
 
@@ -777,8 +785,6 @@ update_ip_idle (gpointer user_data)
 
   gtk_label_set_text (GTK_LABEL (self->waydroid_ip_label), self->waydroid_ip_output);
 
-  g_free (self->waydroid_ip_output);
-
   return G_SOURCE_REMOVE;
 }
 
@@ -875,6 +881,11 @@ update_app_list_idle (gpointer user_data)
   AdwExpanderRow *expander_row = ADW_EXPANDER_ROW (self->app_selector);
   GtkListBox *list_box = self->app_list;
 
+  if (self->apps == NULL) {
+    g_debug ("self->apps became NULL, returning");
+    return G_SOURCE_REMOVE;
+  }
+
   if (!list_box) {
     list_box = GTK_LIST_BOX (gtk_list_box_new ());
     gtk_widget_set_margin_top (GTK_WIDGET (list_box), 8);
@@ -900,8 +911,6 @@ update_app_list_idle (gpointer user_data)
       g_debug ("Added row for app: %s", *app);
     }
   }
-
-  g_strfreev (self->apps);
 
   return G_SOURCE_REMOVE;
 }
@@ -1002,8 +1011,6 @@ update_vendor_idle (gpointer user_data)
 
   gtk_label_set_text (GTK_LABEL (self->waydroid_vendor_label), self->waydroid_vendor_output);
 
-  g_free (self->waydroid_vendor_output);
-
   return G_SOURCE_REMOVE;
 }
 
@@ -1031,8 +1038,6 @@ update_version_idle (gpointer user_data)
   CcWaydroidPanel *self = (CcWaydroidPanel *) user_data;
 
   gtk_label_set_text (GTK_LABEL (self->waydroid_version_label), self->waydroid_version_output);
-
-  g_free (self->waydroid_version_output);
 
   return G_SOURCE_REMOVE;
 }
@@ -1434,6 +1439,10 @@ cc_waydroid_panel_init (CcWaydroidPanel *self)
 
   self->selected_app_name = NULL;
   self->selected_app_pkgname = NULL;
+  self->waydroid_ip_output = NULL;
+  self->waydroid_vendor_output = NULL;
+  self->waydroid_version_output = NULL;
+  self->apps = NULL;
 
   if (g_file_test ("/usr/bin/waydroid", G_FILE_TEST_EXISTS)) {
     g_signal_connect (G_OBJECT (self->waydroid_enabled_switch), "state-set", G_CALLBACK (cc_waydroid_panel_enable_waydroid), self);

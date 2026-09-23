0588d0f3059df30ebf8652e5a3eec517a28319ae
Bardia Moshiri <bardia@furilabs.com>
2026-09-23 17:25:24 -0400

    FIXME: never show automatic suspend notice
    Signed-off-by: Bardia Moshiri <bardia@furilabs.com>

--- panels/power/cc-power-panel.c
+++ panels/power/cc-power-panel.c
@@ -597,6 +597,7 @@ update_suspend_notice_visibility (CcPowerPanel *self)
     gboolean supported, enabled;
 
     supported = self->can_suspend == ACTION_AUTOMATIC && g_strcmp0 (self->chassis_type, "vm") != 0;
+    supported = FALSE;
 
     enabled = adw_switch_row_get_active (self->suspend_on_ac_switch_row);
     if (enabled && self->has_batteries)

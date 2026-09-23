ca59750a6467813af7c7170d9040dd36935a6f3a
Bardia Moshiri <bardia@furilabs.com>
2026-09-23 17:44:48 -0400

    about: add custom methods for getting cpu and disk info then fallback to already existing methods
    we need this because udisks can't get our disk correctly cpuinfo does not work the same way it does on x86
    
    Signed-off-by: Bardia Moshiri <bardia@furilabs.com>

--- panels/system/about/cc-about-page.c
+++ panels/system/about/cc-about-page.c
@@ -412,6 +412,21 @@ get_os_type (void)
 }
 
 char *
+furios_get_primary_disk_info (void)
+{
+    struct statvfs stat;
+    if (statvfs ("/", &stat) != 0) {
+        g_printerr ("Error getting disk information\n");
+        return NULL;
+    }
+
+    guint64 total_size = stat.f_blocks * stat.f_frsize;
+    gdouble total_size_gb = (gdouble)total_size / (1024 * 1024 * 1024);
+
+    return g_strdup_printf ("%.2f GB", total_size_gb);
+}
+
+char *
 get_primary_disk_info (void)
 {
     g_autoptr(UDisksClient) client = NULL;
@@ -525,6 +540,42 @@ get_kernel_version_string ()
         return NULL;
 
     return g_strdup_printf ("%s %s", kernel_name, kernel_release);
+}
+
+char *
+furios_get_cpu_info ()
+{
+    gchar * content = NULL;
+    GError * error = NULL;
+
+    if (g_file_get_contents ("/usr/lib/furios/device/cpuinfo", &content, NULL, &error))
+        return g_strstrip (content);
+
+    g_clear_error (&error);
+
+    if (!g_file_get_contents ("/proc/cpuinfo", &content, NULL, &error)) {
+        g_printerr ("Error reading file: %s\n", error->message);
+        g_error_free (error);
+        return NULL;
+    }
+
+    gchar ** lines = g_strsplit (content, "\n", -1);
+    g_free (content);
+
+    for (gchar ** line = lines; *line != NULL; line++) {
+        if (g_str_has_prefix (*line, "Hardware")) {
+            gchar ** parts = g_strsplit (*line, ":", 2);
+            if (parts[1] != NULL) {
+                gchar * hardware = g_strstrip (g_strdup (parts[1]));
+                g_strfreev (parts);
+                g_strfreev (lines);
+                return hardware;
+            }
+            g_strfreev (parts);
+        }
+    }
+    g_strfreev (lines);
+    return NULL;
 }
 
 char *
@@ -800,7 +851,9 @@ on_copy_row_activated_cb (GtkWidget *widget, CcAboutPage *self)
 
     g_string_append (result_str, "- ");
     system_details_window_title_print_padding ("**Processor:**", result_str, 0);
-    cpu_text = get_cpu_info ();
+    cpu_text = furios_get_cpu_info ();
+    if (cpu_text == NULL)
+        cpu_text = get_cpu_info ();
     g_string_append_printf (result_str, "%s\n", cpu_text);
 
     graphics_hardware_list = get_graphics_hardware_list ();
@@ -822,7 +875,12 @@ on_copy_row_activated_cb (GtkWidget *widget, CcAboutPage *self)
 
     g_string_append (result_str, "- ");
     system_details_window_title_print_padding ("**Disk Capacity:**", result_str, 0);
-    disk_capacity_string = get_primary_disk_info ();
+    disk_capacity_string = furios_get_primary_disk_info ();
+    if (disk_capacity_string == NULL) {
+        disk_capacity_string = get_primary_disk_info ();
+        if (disk_capacity_string == NULL)
+            disk_capacity_string = disk_capacity_string = g_strdup (_("Unknown"));
+    }
     g_string_append_printf (result_str, "%s\n", disk_capacity_string);
 
     g_string_append (result_str, "\n");
@@ -900,15 +958,20 @@ cc_about_page_setup_overview (CcAboutPage *self)
     memory_text = g_format_size_full (ram_size, G_FORMAT_SIZE_IEC_UNITS);
     adw_action_row_set_subtitle (self->memory_row, memory_text);
 
-    cpu_text = get_cpu_info ();
+    cpu_text = furios_get_cpu_info ();
+    if (cpu_text == NULL)
+        cpu_text = get_cpu_info ();
     adw_action_row_set_subtitle (self->processor_row, cpu_text);
 
     graphics_hardware_list = get_graphics_hardware_list ();
     create_graphics_rows (self, graphics_hardware_list);
 
-    disk_capacity_string = get_primary_disk_info ();
-    if (disk_capacity_string == NULL)
-        disk_capacity_string = g_strdup (_("Unknown"));
+    disk_capacity_string = furios_get_primary_disk_info ();
+    if (disk_capacity_string == NULL) {
+        disk_capacity_string = get_primary_disk_info ();
+        if (disk_capacity_string == NULL)
+            disk_capacity_string = disk_capacity_string = g_strdup (_("Unknown"));
+    }
     adw_action_row_set_subtitle (self->disk_row, disk_capacity_string);
 
     os_name_text = get_os_name ();
